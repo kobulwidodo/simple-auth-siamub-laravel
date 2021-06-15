edc8bc343a45042923422a12fd78eb168e87f680
kobulwidodo <fadhilhan01@gmail.com>
2021-06-15 18:58:37 +0700

    Add detail gpa

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,9 +9,9 @@
         <div class="row mt-5">
             <div class="card">
                 <div class="card-body">
-                    <form method="POST" action="/auth">
+                    <form method="POST" action="/">
                         @csrf
-                        <h3 class="text-center">SIAM LOGIN</h3>
+                        <h3 class="text-center">SIAM DATA CHECKER</h3>
                         <div class="mb-3 mt-3">
                             <label for="inputNim" class="form-label">NIM</label>
                             <input type="number" class="form-control" id="inputNim" name="nim">
@@ -55,6 +55,7 @@
                                 <th scope="col">Matkul</th>
                                 <th scope="col">SKS</th>
                                 <th scope="col">Nilai</th>
+                                <th scope="col">Detail</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -65,11 +66,50 @@
                                     <td>{{ $gpa['matkul'] }}</td>
                                     <td>{{ $gpa['sks'] }}</td>
                                     <td>{{ $gpa['nilai'] }}</td>
+                                    <td>
+                                        <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#detailModal{{ $gpa['no'] }}">
+                                            Tampilkan
+                                        </button>
+                                    </td>
                                 </tr>
                                 @endforeach
                             </tbody>
                         </table>
                     </div>
+                    @foreach ($data['data']['gpa'] as $gpa)
+                        <div class="modal fade" id="detailModal{{ $gpa['no'] }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Detail Nilai {{ $gpa['matkul'] }}</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <table class="table">
+                                            <thead>
+                                                <tr>
+                                                <th scope="col">#</th>
+                                                <th scope="col">Tipe Nilai</th>
+                                                <th scope="col">Nilai Ke</th>
+                                                <th scope="col">Nilai</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($data['data']['gpa'][$gpa['no']-1]['detail_nilai'] as $item)
+                                                    <tr>
+                                                        <th scope="row">{{ $item['no'] }}</th>
+                                                        <td>{{ $item['tipe_nilai'] }}</td>
+                                                        <td>{{ $item['nilai_ke'] }}</td>
+                                                        <td>{{ $item['nilai'] }}</td>
+                                                    </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
                     @else
                         <p>{{ $data['msg'] }}</p>
                     @endif
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [AuthController::class, 'index']);
-Route::post('/auth', [AuthController::class, 'auth']);
+Route::post('/', [AuthController::class, 'auth']);
